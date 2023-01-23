8db41a2c1a2897c9c8d0d42bca26be5769d5795f
indamutsa <arsichizy@gmail.com>
2023-01-23 14:31:35 +0100

    Correct issues - openapi 3.0

--- persistence-api/startup/routes.js
+++ persistence-api/startup/routes.js
@@ -28,7 +28,7 @@ const options = {
       title: "MDEForge Persistence API documentation",
       version: "1.0.0",
       description:
-        "This is the documentation of persistence api of MDEForge repository! It uses openapi 3.0 specification",
+        "This is the documentation of persistence api of MDEForge repository! It uses OpenAPI 3.0 specification",
     },
     servers: [
       {
