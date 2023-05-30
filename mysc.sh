e35a6743d0a2172aa03b77b7385c53783d1e4018
indamutsa <arsichizy@gmail.com>
2023-05-30 15:05:10 +0200

    cleaned the repo

--- mysc.sh
+++
@@ -1,2 +0,0 @@
-echo "The server failed to start, please login and fix it" >> he.txt
-echo "The server failed to start, please login and fix it" | mailx -r "Server notification" -s "Elasticsearch notification" arsene@indamutsa.net,indamutsa@mail.com
