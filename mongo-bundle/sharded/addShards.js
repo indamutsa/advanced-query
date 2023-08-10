7fbda65aaacb19a7e7bd203326392fa7e83d769b
indamutsa <arsichizy@gmail.com>
2023-08-10 14:08:15 +0200

    Added a docker-compose with elasticsearch master / worker cluster enabled...

--- mongo-bundle/sharded/addShards.js
+++
@@ -1,6 +0,0 @@
-sh.addShard("mongors1/mongors1n1:27017,mongors1n2:27017,mongors1n3:27017");
-sh.addShard("mongors2/mongors2n1:27017,mongors2n2:27017,mongors2n3:27017");
-sh.enableSharding("testDb");
-db.createCollection("kittens");
-db.kittens.createIndex({ name: "hashed" });
-sh.shardCollection("testDb.kittens", { name: "hashed" });
--- run.sh
+++ run.sh
@@ -57,6 +57,6 @@ echo '####################################################'
 echo 'Spinning the cluster ...'
 echo '####################################################'
 
-# docker-compose up -d --build --force-recreate
+docker-compose up -d --build --force-recreate
 # sleep 45 | echo sleeping --- Connecting the shards to the mongos router ---------
 # docker exec -it mongos1 bash -c "cat /home/app/addShards.js  | mongo "
